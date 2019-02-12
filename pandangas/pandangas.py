38c0a40f78f2b2b9925b053edccbd2c8daaad8e6
Pablo Puerto <pablo.puerto@crem.ch>
2019-02-12 15:13:00 +0100

    copy old project

--- pandangas/pandangas.py
+++ pandangas/pandangas.py
@@ -1,3 +1,226 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import pandas as pd
+
+class _Network:
+
+    # TODO: add H2/CH4 composition
+    LEVELS = {"HP": 5.5e5, "MP": 2.0e5, "BP+": 1.1e5, "BP": 1.025e5}  # Pa
+    LHV = 38.1e3  # kJ/kg
+    V_MAX = 2.0  # m/s
+    T_GRND = 10 + 273.15  # K
+
+    def __init__(self):
+
+        self.bus = pd.DataFrame(columns=["name", "level", "zone", "type"])
+        self.pipe = pd.DataFrame(
+            columns=["name", "from_bus", "to_bus", "length_m", "diameter_m", "material", "in_service"]
+        )
+        self.load = pd.DataFrame(columns=["name", "bus", "p_kW", "min_p_Pa", "scaling"])
+        self.feeder = pd.DataFrame(columns=["name", "bus", "p_lim_kW", "p_Pa"])
+        self.station = pd.DataFrame(columns=["name", "bus_high", "bus_low", "p_lim_kW", "p_Pa"])
+
+        self.res_bus = pd.DataFrame(columns=["name", "p_Pa", "p_bar"])
+        self.res_pipe = pd.DataFrame(columns=["name", "m_dot_kg/s", "v_m/s", "p_kW", "loading_%"])
+        self.res_feeder = pd.DataFrame(columns=["name", "m_dot_kg/s", "p_kW", "loading_%"])
+        self.res_station = pd.DataFrame(columns=["name", "m_dot_kg/s", "p_kW", "loading_%"])
+
+        self.keys = {"bus", "pipe", "load", "feeder", "station", "res_bus", "res_pipe", "res_feeder", "res_station"}
+
+    def __repr__(self):
+        r = "This pandangas network includes the following parameter tables:"
+        par = []
+        res = []
+        for tb in self.keys:
+            if len(getattr(self, tb)) > 0:
+                if "res_" in tb:
+                    res.append(tb)
+                else:
+                    par.append(tb)
+        for tb in par:
+            length = len(getattr(self, tb))
+            r += "\n   - %s (%s %s)" % (tb, length, "elements" if length > 1 else "element")
+        if res:
+            r += "\n and the following results tables:"
+            for tb in res:
+                length = len(getattr(self, tb))
+                r += "\n   - %s (%s %s)" % (tb, length, "elements" if length > 1 else "element")
+
+        return r
+
+
+def _try_existing_bus(net, bus):
+    """
+    Check if a bus exist on a given network, raise ValueError and log an error if not
+
+    :param net: the given network
+    :param bus: the bus to check existence
+    :return:
+    """
+    try:
+        assert bus in net.bus.name.unique()
+    except AssertionError:
+        msg = "The bus {} does not exist !".format(bus)
+        raise ValueError(msg)
+
+
+def _check_level(net, bus_a, bus_b, same=True):
+    """
+    Check the pressure level of two buses on a given network, raise ValueError and log an error depending on parameter
+    when creating a pipe or a station
+
+    :param net: the given network
+    :param bus_a: the first bus
+    :param bus_b: the second bus
+    :param same: if True, the method will check if the node have the same pressure level
+    if False, the method will check if the node have different pressure levels (default: True)
+    :return:
+    """
+    lev_a = net.bus.loc[net.bus.name == bus_a, "level"].all()
+    lev_b = net.bus.loc[net.bus.name == bus_b, "level"].all()
+
+    if same:
+        try:
+            assert lev_a == lev_b
+        except AssertionError:
+            msg = "The buses {} and {} have a different pressure level !".format(bus_a, bus_b)
+            raise ValueError(msg)
+    else:
+        try:
+            assert lev_a != lev_b
+        except AssertionError:
+            msg = "The buses {} and {} have the same pressure level !".format(bus_a, bus_b)
+            raise ValueError(msg)
+
+
+def _change_bus_type(net, bus, bus_type):
+    idx = net.bus.index[net.bus["name"] == bus].tolist()[0]
+    old_type = net.bus.at[idx, "type"]
+    try:
+        assert old_type == "NODE"
+    except AssertionError:
+        msg = "The buses {} is already a {} !".format(bus, old_type)
+        raise ValueError(msg)
+
+    net.bus.at[idx, "type"] = bus_type
+
+
+def create_empty_network():
+    """
+    Create an empty network
+
+    :return: a Network object that will later contain all the buses, pipes, etc.
+    """
+    return _Network()
+
+
+def create_bus(net, level, name, zone=None):
+    """
+    Create a bus on a given network
+
+    :param net: the given network
+    :param level: nominal pressure level of the bus
+    :param name: name of the bus
+    :param zone: zone of the bus (default: None)
+    :return: name of the bus
+    """
+    try:
+        assert level in net.LEVELS
+    except AssertionError:
+        msg = "The pressure level of the bus {} is not in {}".format(name, net.LEVELS)
+        raise ValueError(msg)
+
+    idx = len(net.bus.index)
+    net.bus.loc[idx] = [name, level, zone, "NODE"]
+    return name
+
+
+# TODO: add pipe material into pipe creation and simulation
+def create_pipe(net, from_bus, to_bus, length_m, diameter_m, name, material="steel", in_service=True):
+    """
+    Create a pipe between two existing buses on a given network
+
+    :param net: the given network
+    :param from_bus: the name of the already existing bus where the pipe starts
+    :param to_bus: the name of the already existing bus where the pipe ends
+    :param length_m: length of the pipe (in [m])
+    :param diameter_m: inner diameter of the pipe (in [m])
+    :param name: name of the pipe
+    :param material: material of the pipe
+    :param in_service: if False, the simulation will not take this pipe into account (default: True)
+    :return: name of the pipe
+    """
+    _try_existing_bus(net, from_bus)
+    _try_existing_bus(net, to_bus)
+    _check_level(net, from_bus, to_bus)
+
+    idx = len(net.pipe.index)
+    net.pipe.loc[idx] = [name, from_bus, to_bus, length_m, diameter_m, material, in_service]
+    return name
+
+
+def create_load(net, bus, p_kW, name, min_p_Pa=0.022e5, scaling=1.0):
+    """
+    Create a load attached to an existing bus in a given network
+
+    :param net: the given network
+    :param bus: the existing bus
+    :param p_kW: power consumed by the load (in [kW])
+    :param name: name of the load
+    :param min_p_Pa: minimum acceptable pressure
+    :param scaling: scaling factor for the load (default: 1.0)
+    :return: name of the load
+    """
+    _try_existing_bus(net, bus)
+
+    idx = len(net.load.index)
+    net.load.loc[idx] = [name, bus, p_kW, min_p_Pa, scaling]
+
+    _change_bus_type(net, bus, "SINK")
+    return name
+
+
+def create_feeder(net, bus, p_lim_kW, p_Pa, name):
+    """
+    Create a feeder attached to an existing bus in a given network
+
+    :param net: the given network
+    :param bus: the existing bus
+    :param p_lim_kW: maximum power flowing through the feeder
+    :param p_Pa: operating pressure level at the output of the feeder
+    :param name: name of the feeder
+    :return: name of the feeder
+    """
+    _try_existing_bus(net, bus)
+
+    idx = len(net.feeder.index)
+    net.feeder.loc[idx] = [name, bus, p_lim_kW, p_Pa]
+
+    _change_bus_type(net, bus, "SRCE")
+    return name
+
+
+def create_station(net, bus_high, bus_low, p_lim_kW, p_Pa, name):
+    """
+    Create a pressure station between two existing buses on different pressure level in a given network
+
+    :param net: the given network
+    :param bus_high: the existing bus with higher nominal pressure
+    :param bus_low: the existing bus with lower nominal pressure
+    :param p_lim_kW: maximum power flowing through the feeder
+    :param p_Pa: operating pressure level at the output of the feeder
+    :param name: name of the station
+    :return: name of the station
+    """
+    _try_existing_bus(net, bus_high)
+    _try_existing_bus(net, bus_low)
+    _check_level(net, bus_high, bus_low, same=False)
+
+    idx = len(net.station.index)
+    net.station.loc[idx] = [name, bus_high, bus_low, p_lim_kW, p_Pa]
+
+    _change_bus_type(net, bus_high, "SINK")
+    _change_bus_type(net, bus_low, "SRCE")
+    return name
--- tests/test_pandangas.py
+++ tests/test_pandangas.py
@@ -5,21 +5,89 @@
 
 import pytest
 
+from pandangas import pandangas as pg
 
-from pandangas import pandangas
+@pytest.fixture()
+def fix_create():
+    net = pg.create_empty_network()
+
+    busf = pg.create_bus(net, level="MP", name="BUSF")
+    bus0 = pg.create_bus(net, level="MP", name="BUS0")
+
+    bus1 = pg.create_bus(net, level="BP", name="BUS1")
+    bus2 = pg.create_bus(net, level="BP", name="BUS2")
+    bus3 = pg.create_bus(net, level="BP", name="BUS3")
+
+    pg.create_load(net, bus2, p_kW=10.0, name="LOAD2")
+    pg.create_load(net, bus3, p_kW=15.0, name="LOAD3")
+
+    pg.create_pipe(net, busf, bus0, length_m=1000, diameter_m=0.05, name="PIPE0")
+    pg.create_pipe(net, bus1, bus2, length_m=4000, diameter_m=0.05, name="PIPE1")
+    pg.create_pipe(net, bus1, bus3, length_m=5000, diameter_m=0.05, name="PIPE2")
+    pg.create_pipe(net, bus2, bus3, length_m=3000, diameter_m=0.05, name="PIPE3")
+
+    pg.create_station(net, bus0, bus1, p_lim_kW=50, p_Pa=1.022e5, name="STATION")
+    pg.create_feeder(net, busf, p_lim_kW=50, p_Pa=4.5e5, name="FEEDER")
+
+    return net
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+def test_len_of_created_df(fix_create):
+    net = fix_create
+    assert len(net.bus.index) == 5
+    assert len(net.pipe.index) == 4
+    assert len(net.load.index) == 2
+    assert len(net.feeder.index) == 1
+    assert len(net.station.index) == 1
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+def test_columns_of_created_df(fix_create):
+    net = fix_create
+    assert set(net.bus.columns) == {"name", "level", "zone", "type"}
+    assert set(net.pipe.columns) == {"name", "from_bus", "to_bus", "length_m", "diameter_m", "material", "in_service"}
+    assert set(net.load.columns) == {"name", "bus", "p_kW", "min_p_Pa", "scaling"}
+    assert set(net.feeder.columns) == {"name", "bus", "p_lim_kW", "p_Pa"}
+    assert set(net.station.columns) == {"name", "bus_high", "bus_low", "p_lim_kW", "p_Pa"}
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_bus_creation_bad_bus_level_raise_exception(fix_create):
+    net = fix_create
+    with pytest.raises(ValueError) as e_info:
+        pg.create_bus(net, level="XX", name="BUSX")
+        msg = "The pressure level of the bus BUSX is not in {}".format(["HP", "MP", "BP+", "BP"])
+        assert e_info.value.message == msg
+    assert len(net.bus.index) == 5
+
+
+def test_pipe_creation_non_existing_bus_raise_exception(fix_create):
+    net = fix_create
+    with pytest.raises(ValueError) as e_info:
+        pg.create_pipe(net, "BUS1", "BUSX", length_m=100, diameter_m=0.05, name="PIPEX")
+        msg = "The bus {} does not exist !".format("BUSX")
+        assert e_info.value.message == msg
+    assert len(net.pipe.index) == 4
+
+
+def test_pipe_creation_different_levels_raise_exception(fix_create):
+    net = fix_create
+    with pytest.raises(ValueError) as e_info:
+        pg.create_pipe(net, "BUS0", "BUS1", length_m=100, diameter_m=0.05, name="PIPEX")
+        msg = "The buses BUS0 and BUS1 have a different pressure level !"
+        assert e_info.value.message == msg
+    assert len(net.pipe.index) == 4
+
+
+def test_station_creation_same_level_raise_exception(fix_create):
+    net = fix_create
+    with pytest.raises(ValueError) as e_info:
+        pg.create_station(net, "BUS2", "BUS3", p_lim_kW=50, p_Pa=0.025e5, name="STATION")
+        msg = "The buses BUS2 and BUS3 have the same pressure level !"
+        assert e_info.value.message == msg
+    assert len(net.station.index) == 1
+
+
+def test_network_repr(fix_create):
+    net = fix_create
+    assert "This pandangas network includes the following parameter tables:" in repr(net)
+    assert "- bus (5 elements)" in repr(net)
+    assert "and the following results tables:" not in repr(net)
