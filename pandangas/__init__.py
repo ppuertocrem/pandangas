055e272fcc4d054474032ba72adc2600bd7c4f43
Pablo Puerto <pablo.puerto@crem.ch>
2019-02-13 10:31:40 +0100

    Bump version: 0.1.0 → 0.1.1

--- pandangas/__init__.py
+++ pandangas/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Puerto Pablo"""
 __email__ = "info@crem.ch"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.1.1'"
 
 from pandangas.pandangas import *
 from pandangas.results import runpp
